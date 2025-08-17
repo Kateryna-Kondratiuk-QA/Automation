a034f1e59db84677f98c0bfe7a6892760534dfc3
Kateryna-Kondratiuk-QA <Kateryna.Kondratiuk@budgetthuis.nl>
2025-08-17 23:37:36 +0200

    Initial commit

---
+++ Task1/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the purchase amount: ");
+
+            bool isValidValue = double.TryParse(Console.ReadLine(), out double amount);
+            if (!isValidValue)
+            {
+                Console.WriteLine("Wrong input");
+                return;
+            }
+
+            if (amount > 5000)
+            {
+                amount = amount - ((amount * 10) / 100);
+                Console.WriteLine($"Your discount is 10%! Payable amount: {amount}");
+            }
+            else if (amount <= 5000 && amount > 1000)
+            {
+                amount = amount - ((amount * 5) / 100);
+                Console.WriteLine($"Your discount is 5%! Payable amount: {amount}");
+            }
+            else
+            {
+                Console.WriteLine("Unfortunately, you don't have a discount");
+            }
+
+
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the temperature: ");
+            bool isValid = int.TryParse(Console.ReadLine(), out int temperature);
+            if (!isValid)
+            {
+                Console.WriteLine("Wrong input");
+                return;
+            }
+       
+            if (temperature < 15)
+            {
+                Console.WriteLine("Put on a warm clothes");
+            }
+            else if (temperature >= 15 && temperature <= 25)
+            {
+                Console.WriteLine("Put on a light jacket");
+            }
+            else
+            {
+                Console.WriteLine("Put on a T-shirt and shorts");
+            }           
+        }
+    }
+}
