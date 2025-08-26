a5ff2ecc2fb790c925ee497872caa1d20be84f7a
Kateryna-Kondratiuk-QA <Kateryna.Kondratiuk@budgetthuis.nl>
2025-08-26 17:17:03 +0200

    Adding task8 and task10

---
+++ Task10/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Task10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool runningProgram = true;
+            double balance = 1000;
+
+            while (runningProgram)
+            {
+                Console.WriteLine("Menu" +
+                    "\n1. Top up balance" +
+                    "\n2. Withdraw money" +
+                    "\n3. Check balance" +
+                    "\n4. Exit");
+
+                bool isValidValue = int.TryParse(Console.ReadLine(), out int choice);
+                if (!isValidValue)
+                {
+                    Console.WriteLine("Wrong input");
+                    continue;
+
+                }
+                else if (choice <= 0 || choice > 4)
+                {
+                    Console.WriteLine("Wrong number");
+                    continue;
+                }
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter sum: ");
+                        double amountToTopUp = Convert.ToDouble(Console.ReadLine());
+                        balance = balance + amountToTopUp;
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter sum to withdraw: ");
+                        double amountToWithdraw = Convert.ToDouble(Console.ReadLine());
+                        balance = balance - amountToWithdraw;
+                        break;
+                    case 3:
+                        Console.WriteLine($"Your balance is {balance}");
+                        break;
+                    case 4:
+                        Console.WriteLine("Bye");
+                        runningProgram = false;
+                        break;
+                }
+
+                Console.WriteLine("Press any key to continue");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
---
+++ Task8/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Task8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter word: ");
+            string word = Console.ReadLine();
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (!char.IsLetter(word[i]))
+                {
+                    Console.WriteLine("Wrong input");
+                    return;
+                }
+            }
+
+            char[] wordArray = word.ToCharArray();
+            char[] reversedWordArray = word.ToCharArray();
+            Array.Reverse(reversedWordArray);
+
+            for (int i = 0; i < wordArray.Length; i++)
+            {
+                if (wordArray[i] == reversedWordArray[i])
+                {
+                    Console.Write(reversedWordArray[i] + " ");
+                }
+                else
+                {
+                    Console.WriteLine("Your word is not palindrome");
+                    return;
+                }
+            }
+
+            Console.WriteLine("\nYour word is palindrome");
+
+        }
+    }
+}
